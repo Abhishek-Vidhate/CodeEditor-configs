725e0a65b45294fbf6eb0b839aad6a72faa33029
Abhishek Vidhate <abhishek.ai.dev@gmail.com>
2025-07-10 06:09:25 +0530

    initialization

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,86 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- LSP Server to use for Rust.
+-- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
+-- only for diagnostics. The rest of LSP support will still be
+-- provided by rust-analyzer.
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+
+opts = {
+  completion = {
+    crates = {
+      enabled = true,
+    },
+  },
+  lsp = {
+    enabled = true,
+    actions = true,
+    completion = true,
+    hover = true,
+  },
+}
+
+opts = { ensure_installed = { "rust", "ron" } }
+
+opts = {
+  server = {
+    on_attach = function(_, bufnr)
+      vim.keymap.set("n", "<leader>cR", function()
+        vim.cmd.RustLsp("codeAction")
+      end, { desc = "Code Action", buffer = bufnr })
+      vim.keymap.set("n", "<leader>dr", function()
+        vim.cmd.RustLsp("debuggables")
+      end, { desc = "Rust Debuggables", buffer = bufnr })
+    end,
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ["rust-analyzer"] = {
+        cargo = {
+          allFeatures = true,
+          loadOutDirsFromCheck = true,
+          buildScripts = {
+            enable = true,
+          },
+        },
+        -- Add clippy lints for Rust if using rust-analyzer
+        checkOnSave = diagnostics == "rust-analyzer",
+        -- Enable diagnostics if using rust-analyzer
+        diagnostics = {
+          enable = diagnostics == "rust-analyzer",
+        },
+        procMacro = {
+          enable = true,
+          ignored = {
+            ["async-trait"] = { "async_trait" },
+            ["napi-derive"] = { "napi" },
+            ["async-recursion"] = { "async_recursion" },
+          },
+        },
+        files = {
+          excludeDirs = {
+            ".direnv",
+            ".git",
+            ".github",
+            ".gitlab",
+            "bin",
+            "node_modules",
+            "target",
+            "venv",
+            ".venv",
+          },
+        },
+      },
+    },
+  },
+}
+
+opts = {
+  servers = {
+    bacon_ls = {
+      enabled = diagnostics == "bacon-ls",
+    },
+    rust_analyzer = { enabled = false },
+  },
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# My current Neovim and Ghostty terminal configurations
+
+basically used tokyo night preset from starship prompt , and Lazyvim for Neovim config management.
+
+i only know somewhat of Rust development so have only added Rust plugins and LSP for simple and clean development. and this just works so i am happay for now.
+
+NOTE :
+
+you'll have to add starship prompt to path ( in .zshrc or .zprofile) .
+
+check installation guide for both neovim and starship prompts , easy to install , just follow 
